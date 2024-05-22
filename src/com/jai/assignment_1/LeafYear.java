6847ca44fb07983a820d62157ac628c78d33560f
jayak4 <jayakumaraws7@gmail.com>
2024-05-22 09:33:39 +0530

    adding leaf year program

---
+++ src/com/jai/assignment_1/LeafYear.java
@@ -0,0 +1,19 @@
+package com.jai.assignment_1;
+
+import java.util.Scanner;
+
+public class LeafYear {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please Input the year:");
+        int year = in.nextInt();
+        if (year % 4 == 0)
+        {
+            System.out.println(year+" is a leaf year.!");
+        }
+        else
+        {
+            System.out.println(year+" is not a leaf year.!");
+        }
+    }
+}
